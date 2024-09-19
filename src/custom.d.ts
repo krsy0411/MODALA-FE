9962340dd6fe17eed725310b641c8e7b7d1289e2
PANdas <krsy0411@naver.com>
2024-09-19 20:16:01 +0900

    환경변수 test

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,11 +1,11 @@
 export {};
 
-declare module '*.svg' {
-  const content: any;
+declare module "*.svg" {
+	const content: any;
 }
 
 declare global {
-  interface Window {
-    kakao: any;
-  }
+	interface Window {
+		kakao: any;
+	}
 }
--- src/login/components/SocialLoginButton.tsx
+++ src/login/components/SocialLoginButton.tsx
@@ -1,40 +1,49 @@
-import * as Styled from '../css/social.button.styled';
+import * as Styled from "../css/social.button.styled";
 
 interface ButtonProps {
-  socialName: string;
-  backgroundColor: string;
-  message: string;
+	socialName: string;
+	backgroundColor: string;
+	message: string;
 }
-export default function SocialLoginButton({ socialName, backgroundColor, message }: ButtonProps) {
-  const link = `${import.meta.env.VITE_BE_URL}/auth/kakao/login`;
+export default function SocialLoginButton({
+	socialName,
+	backgroundColor,
+	message,
+}: ButtonProps) {
+	const link = `${import.meta.env.PROD.VITE_BE_URL}/auth/kakao/login`;
 
-  async function goRedirection() {
-    if (socialName === 'kakao') {
-      window.location.href = link;
-    }
-  }
+	async function goRedirection() {
+		if (socialName === "kakao") {
+			window.location.href = link;
+		}
+	}
 
-  return (
-    <>
-      <div>
-        <Styled.Button $backgroundcolor={backgroundColor} onClick={goRedirection}>
-          <Styled.LogoImg
-            src={`/svg/login/${socialName}.logo.svg`}
-            alt={`${socialName}-logo`}
-            className="logo"
-          />
-          <div
-            style={{
-              display: 'flex',
-              flex: '1',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Styled.LoginText $fontcolor={socialName}>{message}</Styled.LoginText>
-          </div>
-        </Styled.Button>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<Styled.Button
+					$backgroundcolor={backgroundColor}
+					onClick={goRedirection}
+				>
+					<Styled.LogoImg
+						src={`/svg/login/${socialName}.logo.svg`}
+						alt={`${socialName}-logo`}
+						className="logo"
+					/>
+					<div
+						style={{
+							display: "flex",
+							flex: "1",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<Styled.LoginText $fontcolor={socialName}>
+							{message}
+						</Styled.LoginText>
+					</div>
+				</Styled.Button>
+			</div>
+		</>
+	);
 }
